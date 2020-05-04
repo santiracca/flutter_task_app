483068283360fd1941769a3ac487f78b65f2206f
santiracca <santiagoracca7@gmail.com>
2020-05-04 10:39:42 -0400

    Add login screen and email auth from firebase

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,224 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:task_app/config/config.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+
+  final TextEditingController _passwordController = TextEditingController();
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  void _signIn(BuildContext context) async {
+    String email = _emailController.text.trim();
+    String password = _passwordController.text;
+
+    if (email.isNotEmpty && password.isNotEmpty) {
+      try {
+        final AuthResult authResult = await _auth.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        showDialog(
+          context: context,
+          builder: (ctx) {
+            return AlertDialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              title: Text('Done'),
+              content: Text('Signin success'),
+              actions: [
+                FlatButton(
+                  child: Text("Cancel"),
+                  onPressed: () {
+                    Navigator.of(ctx).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      } catch (e) {
+        print(e);
+        showDialog(
+          context: context,
+          builder: (ctx) {
+            return AlertDialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              title: Text('Error'),
+              content: Text('Invalid Login'),
+              actions: [
+                FlatButton(
+                  child: Text("Cancel"),
+                  onPressed: () {
+                    Navigator.of(ctx).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      }
+    } else {
+      showDialog(
+        context: context,
+        builder: (ctx) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            title: Text('Error'),
+            content: Text('Please provide email and password'),
+            actions: [
+              FlatButton(
+                child: Text("Cancel"),
+                onPressed: () {
+                  Navigator.of(ctx).pop();
+                },
+              ),
+              FlatButton(
+                child: Text("OK"),
+                onPressed: () {
+                  _emailController.text = "";
+                  _passwordController.text = "";
+                  Navigator.of(ctx).pop();
+                },
+              )
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.only(bottom: 80),
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 80),
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: Color(0x4400F58D),
+                      blurRadius: 30,
+                      offset: Offset(10, 10),
+                      spreadRadius: 0),
+                ],
+              ),
+              child: Image(
+                image: AssetImage('assets/logo_round.png'),
+                width: 200,
+                height: 200,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              child: Text(
+                'Login',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.only(top: 40),
+              child: TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                  hintText: 'Write Email here',
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.only(top: 10),
+              child: TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                  hintText: 'Write Password here',
+                ),
+                obscureText: true,
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                _signIn(context);
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [primaryColor, secondaryColor],
+                  ),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                child: Center(
+                    child: Text(
+                  'Login With Email',
+                  style: TextStyle(color: Colors.white),
+                )),
+              ),
+            ),
+            FlatButton(
+              onPressed: () {},
+              child: Text('Signup  using Email'),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 30),
+              child: Wrap(
+                children: [
+                  FlatButton.icon(
+                    onPressed: () {},
+                    icon: Icon(
+                      FontAwesomeIcons.google,
+                      color: Colors.red,
+                    ),
+                    label: Text(
+                      'Sign-In with Gmail',
+                      style: TextStyle(
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                  FlatButton.icon(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.phone,
+                      color: Colors.blue,
+                    ),
+                    label: Text(
+                      'Sign-In using Phone',
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
